f5572b41764b0cbaeff5ca895a4ef6db1cc33978
Evgeny Akulenko <gaidin17@yandex.ru>
2017-11-13 00:37:46 +0400

    create pom.xml and some classes

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+/**
+ * Created by Gaidin on 12.11.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/service/service.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by Gaidin on 12.11.2017.
+ */
+public class service {
+}
