98af3603fb051a1a2d03474fafc481cd7e3e4b15
Evgeny Akulenko <gaidin17@yandex.ru>
2017-11-14 20:39:53 +0300

    update com-Port management

---
+++ src/main/java/app/utils/port/decoding/DecodingAlgorithm.java
@@ -0,0 +1,12 @@
+package app.utils.port.decoding;
+
+/**
+ * Created by Evgeny_Akulenko on 11/14/2017.
+ */
+public class DecodingAlgorithm implements Algorithm<Double> {
+
+	@Override
+	public Double calculate() {
+		return null;
+	}
+}
